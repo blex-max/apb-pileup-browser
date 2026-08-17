b0fa7b9e26666e72441460f527403fbfa475f9f9
blex-max <ablex7@gmail.com>
2026-08-17 14:46:55 +0100

    vendor required deps

---
+++ vendor/plog/include/plog/Appenders/ArduinoAppender.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <Arduino.h>
+#include <plog/Appenders/IAppender.h>
+
+namespace plog {
+template <class Formatter>
+class PLOG_LINKAGE_HIDDEN ArduinoAppender : public IAppender {
+ public:
+  ArduinoAppender (Stream& stream) : m_stream (stream) {}
+
+  virtual void write (const Record& record) PLOG_OVERRIDE
+  {
+    m_stream.print (Formatter::format (record).c_str());
+  }
+
+ private:
+  Stream& m_stream;
+};
+}  // namespace plog
